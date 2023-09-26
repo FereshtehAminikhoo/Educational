3bc8260ac12c32b96fadf06f914f8a5b29bd81fb
Fereshteh <faminikhoo@gmail.com>
2023-09-26 17:07:59 +0330

    Adding sending email blade.php for reseting the password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,29 @@
-@extends('layouts.app')
-
+@extends('auth.master')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+        <form action="{{route('password.email')}}" class="form" method="post">
+            @csrf
+            <a class="account-logo" href="/">
+                <img src="/img/weblogo.png" alt="">
+            </a>
+            <div class="form-content form-account">
+                @if(session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{session('status')}}
+                    </div>
+                @endif
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                <input type="email" class="txt-l txt @error('email') is-invalid @enderror" placeholder="ایمیل"
+                name="email" value="{{old('email')}}" required autocomplete="email" autofocus>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{$message}}</strong>
+                </span>
+                @enderror
+                <br>
+                <button class="btn btn-recoverpass" type="submit">بازیابی</button>
             </div>
-        </div>
-    </div>
-</div>
+            <div class="form-footer">
+                <a href="{{route('login')}}">صفحه ورود</a>
+            </div>
+        </form>
 @endsection
