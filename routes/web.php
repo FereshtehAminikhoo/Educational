87c684e21301e369e147da01345f105ee2edc4da
Fereshteh <faminikhoo@gmail.com>
2023-10-04 20:58:46 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 });
 
 //authentication client
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
