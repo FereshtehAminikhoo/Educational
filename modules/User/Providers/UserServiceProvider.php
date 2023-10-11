2fe3fd30ba153d904b512e195681ff8c55126c41
Fereshteh <faminikhoo@gmail.com>
2023-10-11 22:44:07 +0330

    Adding load views address and setting config

--- modules/User/Providers/UserServiceProvider.php
+++ modules/User/Providers/UserServiceProvider.php
@@ -3,11 +3,18 @@
 namespace User\Providers;
 
 use \Illuminate\Support\ServiceProvider;
+use User\Models\User;
 
 class UserServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
+        config()->set('auth.providers.users.model', User::class);
+    }
     public function boot()
     {
-
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/user_routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'User');
     }
 }
