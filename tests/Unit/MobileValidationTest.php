4a6527f2646b2f8f129a1a74dfee0ebd3743bbd3
Fereshteh <faminikhoo@gmail.com>
2023-10-16 23:40:14 +0330

    Creating and adding Mobile and Password unit test

---
+++ tests/Unit/MobileValidationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use User\Rules\ValidMobile;
+
+class MobileValidationTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_mobile_can_not_be_less_than_10_character()
+    {
+        $result = (new ValidMobile())->passes('', '939200202');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_mobile_can_not_be_more_than_10_character()
+    {
+        $result = (new ValidMobile())->passes('', '93920020212');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_mobile_should_start_by_9()
+    {
+        $result = (new ValidMobile())->passes('', '3392002021');
+        $this->assertEquals(0, $result);
+    }
+
+}
---
+++ tests/Unit/PasswordValidationTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use User\Rules\ValidPassword;
+
+class PasswordValidationTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_password_should_not_be_less_than_6_character()
+    {
+        $result = (new ValidPassword())->passes('', 'Aa12!');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_password_should_include_sign_character()
+    {
+        $result = (new ValidPassword())->passes('', 'Aa12dfs');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_password_should_include_digit_character()
+    {
+        $result = (new ValidPassword())->passes('', 'Aambvfs@');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_password_should_include_Capital_character()
+    {
+        $result = (new ValidPassword())->passes('', 'jbvjdbv@');
+        $this->assertEquals(0, $result);
+    }
+
+    public function test_password_should_include_small_character()
+    {
+        $result = (new ValidPassword())->passes('', 'ABJDVKJCHKSJDVF@');
+        $this->assertEquals(0, $result);
+    }
+}
