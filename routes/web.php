a5bca952637dce9ce9f7e6181a16243935457d8a
Fereshteh <faminikhoo@gmail.com>
2023-10-17 21:18:29 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,16 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/verify-link/{user}/', function(){
+    if (request()->hasValidSignature()){
+        return 'ok';
+    }
+    return 'Failed';
+})->name('verify-link');
+
+Route::get('/test', function(){
+    $url = URL::temporarySignedRoute('verify-link', now()->addSeconds(20), ['user' => 5]);
+    dd($url);
+});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
