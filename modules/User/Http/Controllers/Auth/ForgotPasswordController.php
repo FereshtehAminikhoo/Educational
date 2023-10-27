05f736aa07dfc35280a0d9edc1b35c597db53b88
Fereshteh <faminikhoo@gmail.com>
2023-10-27 23:04:07 +0330

    Adding routes for login, register and reset password

--- modules/User/Http/Controllers/Auth/ForgotPasswordController.php
+++ modules/User/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,14 @@
 namespace User\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use User\Http\Requests\ResetPasswordVerifyCodeRequest;
+use User\Http\Requests\sendResetPasswordVerifyCodeRequest;
+use User\Http\Requests\VerifyCodeRequest;
+use User\Models\User;
+use User\Repositories\UserRepo;
+use User\Services\VerifyCodeService;
 
 class ForgotPasswordController extends Controller
 {
@@ -20,8 +27,36 @@ class ForgotPasswordController extends Controller
 
     use SendsPasswordResetEmails;
 
-    public function showLinkRequestForm()
+
+    public function showVerifyCodeRequestForm()
     {
         return view('User::Front.passwords.email');
+    }
+
+    public function sendVerifyCodeEmail(sendResetPasswordVerifyCodeRequest $request)
+    {
+        // check if exists in database
+        $user = resolve(UserRepo::class)->findByEmail($request);
+
+        if ($user && ! VerifyCodeService::has($user->id)){
+            $user -> sendResetPasswordRequestNotification();
+        }
+
+
+        return view('User::Front.passwords.enter-verify-code');
+    }
+
+    public function checkVerifyCode(ResetPasswordVerifyCodeRequest $request)
+    {
+        // check if exists in database
+        $user = resolve(UserRepo::class)->findByEmail($request);
+
+        if(VerifyCodeService::check($user->id, $request->verify_code)){
+           auth()->loginUsingId($user->id);
+           return redirect() -> route('password.showResetForm');
+        }
+
+        return back()->withErrors(['verify_code' => 'کد وارد شده معتبر نمی باشد!']);
+
     }
 }
--- modules/User/Routes/user_routes.php
+++ modules/User/Routes/user_routes.php
@@ -1,8 +1,42 @@
 <?php
+
 //authentication user
+
 Route::group([
     'namespace'=>'User\Http\Controllers',
     'middleware'=>'web'
 ], function ($router) {
     Auth::routes(['verify'=>true]);
+    //verification routes
+    Route::post('/email/verify', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('/email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+    Route::get('/email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+
+    //login routes
+    Route::post('/login', 'Auth\LoginController@login')->name('login');
+    Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+
+    //logout route
+    Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+    //reset password route
+    Route::get('/password/reset', 'Auth\ForgotPasswordController@showVerifyCodeRequestForm')->name('password.request');
+    Route::get('/password/reset/send', 'Auth\ForgotPasswordController@sendVerifyCodeEmail')->name('password.sendVerifyCodeEmail');
+    Route::post('/password/reset/check-verify-code', 'Auth\ForgotPasswordController@checkVerifyCode')->name('password.checkVerifyCode')
+        ->middleware('throttle:5, 1');
+    Route::get('/password/change', 'Auth\ResetPasswordController@showResetForm')->name('password.showResetForm')
+        ->middleware('auth');
+    Route::post('/password/change', 'Auth\ResetPasswordController@reset')->name('password.update');
+    Route::get('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+
+
+    //register routes
+    Route::post('/register', 'Auth\RegisterController@register')->name('register');
+    Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+
+
+
+
+
 });
