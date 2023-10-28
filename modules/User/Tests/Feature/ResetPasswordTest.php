465f6579de04764797ac7fdffc436114c72ac13b
Fereshteh <faminikhoo@gmail.com>
2023-10-28 10:05:39 +0330

    Adding some tests in reset password test

--- modules/User/Tests/Feature/ResetPasswordTest.php
+++ modules/User/Tests/Feature/ResetPasswordTest.php
@@ -23,5 +23,29 @@ class ResetPasswordTest extends TestCase
         $response -> assertOk();
     }
 
+    public function test_user_see_enter_verify_code_form_by_correct_email()
+    {
+        $response = $this->call('get', route('password.sendVerifyCodeEmail'), ['email' => 'test2@gmail.com']);
+        $response -> assertOk();
+    }
+
+    public function test_user_cannot_see_enter_verify_code_form_by_wrong_email()
+    {
+        $response = $this->call('get', route('password.sendVerifyCodeEmail'), ['email' => 'test2.com']);
+        $response -> assertStatus(302);
+    }
+
+    public function test_user_banned_after_6_attempt_to_reset_password()
+    {
+        for ($i=0 ; $i<5; $i++){
+            $response = $this->post(route('password.checkVerifyCode'), ['verify_code', 'email' => 'test2@gmail.com']);
+            $response -> assertStatus(302);
+        }
+
+        $response = $this->post(route('password.checkVerifyCode'), ['verify_code', 'email' => 'test2@gmail.com']);
+        $response -> assertStatus(429);
+
+    }
+
 
 }
