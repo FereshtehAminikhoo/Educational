2345880366f9bb185486755acf5d045a4444e336
Fereshteh <faminikhoo@gmail.com>
2024-01-17 23:09:43 +0330

    Creating service provider and setting super admin permission

---
+++ modules/RolePermissions/Providers/RolePermissionsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace RolePermissions\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+use RolePermissions\Models\Permission;
+
+class RolePermissionsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/role_permissions_routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'RolePermissions');
+        $this->loadJsonTranslationsFrom(__DIR__ . '/../Resources/Lang');
+
+        Gate::before(function($user){
+            return $user->hasPermissionTo(Permission::PERMISSION_SUPER_ADMIN) ? true : null;
+        });
+    }
+
+
+    public function boot()
+    {
+        config()->set('sidebar.items.role-permissions', [
+            "icon" => "i-role-permissions",
+            "title" => "نقش های کاربری",
+            "url" => route('role-permissions.index'),
+        ]);
+    }
+
+}
