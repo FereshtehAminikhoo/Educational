894e6d96915afea8dc29a70f5f48bfa5f9a87010
Fereshteh <faminikhoo@gmail.com>
2024-01-17 23:36:49 +0330

    Adding fields of migration and model

---
+++ modules/Media/Database/Migrations/2024_01_06_120528_create_media_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->json('files');
+            $table->enum('type', ['image', 'video', 'audio', 'zip', 'doc']);
+            $table->string('filename', 255);
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medias');
+    }
+}
---
+++ modules/Media/Models/Media.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Media\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Media\Services\MediaFileService;
+
+class Media extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+
+    protected $fillable = [
+        'user_id',
+        'files',
+        'type',
+        'filename'
+    ];
+
+
+    protected $casts = [
+        'files' => 'json'
+    ];
+
+
+    protected static function booted()
+    {
+        static::deleting(function($media){
+            MediaFileService::delete($media);
+        });
+    }
+
+
+    public function getThumbAttribute()
+    {
+        return '/storage/' . $this->files[300];
+    }
+}
