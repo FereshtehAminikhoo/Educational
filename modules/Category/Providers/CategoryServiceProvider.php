8957ca134faf2eb06a03f2df29f2182a81e971cf
Fereshteh <faminikhoo@gmail.com>
2024-01-17 22:59:26 +0330

    Creating service provider for category module

---
+++ modules/Category/Providers/CategoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Category\Providers;
+
+use Category\Models\Category;
+use Category\Policies\CategoryPolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class CategoryServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/categories_routes.php');
+        $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'Categories');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+        Gate::policy(Category::class, CategoryPolicy::class);
+    }
+
+
+    public function boot()
+    {
+        config()->set('sidebar.items.categories', [
+            "icon" => "i-categories",
+            "title" => "دسته بندی ها",
+            "url" => route('categories.index'),
+        ]);
+    }
+
+}
