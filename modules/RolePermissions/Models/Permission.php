9ca4f4c2c9da4c162bc7811822504c9dde82e9b9
Fereshteh <faminikhoo@gmail.com>
2024-01-19 13:50:56 +0330

    Adding manage users permission

--- modules/RolePermissions/Models/Permission.php
+++ modules/RolePermissions/Models/Permission.php
@@ -16,6 +16,7 @@ class Permission extends \Spatie\Permission\Models\Permission
     const PERMISSION_TEACH = 'teach';
     const PERMISSION_MANAGE_CATEGORIES = 'manage categories';
     const PERMISSION_MANAGE_ROLE_PERMISSIONS = 'manage role_permissions';
+    const PERMISSION_MANAGE_USERS = 'manage users';
     const PERMISSION_MANAGE_COURSES = 'manage courses';
     const PERMISSION_MANAGE_OWN_COURSES = 'manage own courses';
 
@@ -27,6 +28,7 @@ class Permission extends \Spatie\Permission\Models\Permission
         self::PERMISSION_TEACH,
         self::PERMISSION_MANAGE_CATEGORIES,
         self::PERMISSION_MANAGE_ROLE_PERMISSIONS,
+        self::PERMISSION_MANAGE_USERS,
         self::PERMISSION_MANAGE_COURSES,
         self::PERMISSION_MANAGE_OWN_COURSES
     ];
