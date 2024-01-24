afaf242d8c652b7441cd5efc6295e30a7df60fa0
Fereshteh <faminikhoo@gmail.com>
2024-01-24 16:39:52 +0330

    Adding roles

--- modules/RolePermissions/Models/Role.php
+++ modules/RolePermissions/Models/Role.php
@@ -11,9 +11,17 @@ class Role extends \Spatie\Permission\Models\Role
     protected $guarded = [];
 
     const ROLE_TEACHER = 'teacher';
+    const ROLE_SUPER_ADMIN = 'super admin';
+    const ROLE_STUDENT = 'student';
     static $roles = [
         self::ROLE_TEACHER => [
             Permission::PERMISSION_TEACH
+        ],
+        self::ROLE_SUPER_ADMIN => [
+            Permission::PERMISSION_SUPER_ADMIN
+        ],
+        self::ROLE_STUDENT => [
+
         ]
     ];
 }
