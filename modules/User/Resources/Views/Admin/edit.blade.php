17510af7b261deb63c52e360d1e609fd3eb39c48
Fereshteh <faminikhoo@gmail.com>
2024-01-25 16:04:35 +0330

    Deleting some inputs and adding two tables at the end of the page

--- modules/User/Resources/Views/Admin/edit.blade.php
+++ modules/User/Resources/Views/Admin/edit.blade.php
@@ -4,7 +4,7 @@
     <li><a href="#" title="ویرایش کاربر">ویرایش کاربر</a></li>
 @endsection
 @section('content')
-    <div class="row no-gutters ">
+    <div class="row no-gutters margin-bottom-20 ">
         <div class="col-12 bg-white">
             <p class="box__title">بروزرسانی کاربر</p>
             <form action="{{route('users.update', $user->id)}}" method="post" class="padding-30" enctype="multipart/form-data">
@@ -19,18 +19,6 @@
                 <x-input type="number" name="mobile" class="text-left" placeholder="شماره موبایل" required value="{{$user->mobile}}"></x-input>
 
                 <x-input type="text" name="headline" class="text-left" placeholder="عنوان" value="{{$user->headline}}"></x-input>
-
-                <x-input type="text" name="website" class="text-left" placeholder="وب سایت" value="{{$user->website}}"></x-input>
-
-                <x-input type="text" name="linkedin" class="text-left" placeholder="لینکداین" value="{{$user->linkedin}}"></x-input>
-
-                <x-input type="text" name="facebook" class="text-left" placeholder="فیسبوک" value="{{$user->facebook}}"></x-input>
-
-                <x-input type="text" name="twitter" class="text-left" placeholder="توییتر" value="{{$user->twitter}}"></x-input>
-
-                <x-input type="text" name="youtube" class="text-left" placeholder="یوتیوب" value="{{$user->youtube}}"></x-input>
-
-                <x-input type="text" name="instagram" class="text-left" placeholder="اینستاگرام" value="{{$user->instagram}}"></x-input>
 
                 <x-input type="text" name="telegram" class="text-left" placeholder="تلگرام" value="{{$user->telegram}}"></x-input>
 
@@ -50,6 +38,57 @@
                 <br>
                 <button class="btn btn-webamooz_net">بروزرسانی</button>
             </form>
+        </div>
+    </div>
+    <div class="row no-gutters">
+        <div class="col-6 margin-left-10 margin-bottom-20">
+            <p class="box__title">درحال یادگیری</p>
+            <div class="table__box">
+                <table class="table">
+                    <thead role="rowgroup">
+                    <tr role="row" class="title-row">
+                        <th>شناسه</th>
+                        <th>نام دوره</th>
+                        <th>نام مدرس</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr role="row" class="">
+                        <td><a href="">1</a></td>
+                        <td><a href="">دوره لاراول</a></td>
+                        <td><a href="">صیاد اعظمی</a></td>
+                    </tr>
+                    <tr role="row" class="">
+                        <td><a href="">1</a></td>
+                        <td><a href="">دوره لاراول</a></td>
+                        <td><a href="">صیاد اعظمی</a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="col-6 margin-bottom-20">
+            <p class="box__title">دوره های مدرس</p>
+            <div class="table__box">
+                <table class="table">
+                    <thead role="rowgroup">
+                    <tr role="row" class="title-row">
+                        <th>شناسه</th>
+                        <th>نام دوره</th>
+                        <th>نام مدرس</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($user->courses as $course)
+                        <tr role="row" class="">
+                            <td><a href="">{{$course->id}}</a></td>
+                            <td><a href="">{{$course->title}}</a></td>
+                            <td><a href="">{{$course->teacher->name}}</a></td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
