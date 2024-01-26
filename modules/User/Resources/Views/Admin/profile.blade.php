55f33c4967d2e38d372fa0c7e0be92cc20f50c6e
Fereshteh <faminikhoo@gmail.com>
2024-01-26 12:09:34 +0330

    Using @can for some inputs that they are required if user has teach permission

--- modules/User/Resources/Views/Admin/profile.blade.php
+++ modules/User/Resources/Views/Admin/profile.blade.php
@@ -16,25 +16,24 @@
 
                 <x-input type="number" name="mobile" class="text-left" placeholder="شماره موبایل" required value="{{auth()->user()->mobile}}"></x-input>
 
+                <x-input type="password" name="password" placeholder="رمز عبور جدید" value=""></x-input>
+                <p class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای
+                    غیر الفبا مانند <strong>!@#$%^&*()</strong> باشد.</p>
+
+                <x-input type="text" name="telegram" class="text-left" placeholder="تلگرام" value="{{auth()->user()->telegram}}"></x-input>
+
+                @can(\RolePermissions\Models\Permission::PERMISSION_TEACH)
                 <x-input type="text" name="card_number" class="text-left" placeholder="شماره کارت بانکی" value="{{auth()->user()->card_number}}"></x-input>
 
                 <x-input type="text" name="shaba" class="text-left" placeholder="شماره شبا بانکی" value="{{auth()->user()->shaba}}"></x-input>
 
                 <x-input type="text" name="username" class="text-left" placeholder="نام کاربری و آدرس پروفایل" value="{{auth()->user()->username}}"></x-input>
                 <p class="input-help text-left margin-bottom-12" dir="ltr">
-                    {{auth()->user()->profilePath()}}
-                    <a href="{{auth()->user()->profilePath()}}">{{auth()->user()->username}}</a>
+                    <a href="{{auth()->user()->profilePath()}}">{{auth()->user()->profilePath()}}</a>
                 </p>
 
                 <x-input type="text" name="headline" placeholder="عنوان" value="{{auth()->user()->headline}}"></x-input>
 
-                <x-input type="text" name="telegram" class="text-left" placeholder="تلگرام" value="{{auth()->user()->telegram}}"></x-input>
-
-                <x-input type="password" name="password" placeholder="رمز عبور" value=""></x-input>
-                <p class="rules">رمز عبور باید حداقل ۶ کاراکتر و ترکیبی از حروف بزرگ، حروف کوچک، اعداد و کاراکترهای
-                    غیر الفبا مانند <strong>!@#$%^&*()</strong> باشد.</p>
-
-                @can(\RolePermissions\Models\Permission::PERMISSION_TEACH)
                 <x-textarea name="bio" placeholder="بیو" value="{{auth()->user()->bio}}"></x-textarea>
                 @endcan
 
