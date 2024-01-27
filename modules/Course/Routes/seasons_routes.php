48f70d2d199d834cc4d56650d944859cc2b70303
Fereshteh <faminikhoo@gmail.com>
2024-01-27 23:35:49 +0330

    Adding edit and update routes

--- modules/Course/Routes/seasons_routes.php
+++ modules/Course/Routes/seasons_routes.php
@@ -4,4 +4,6 @@ Route::group(['namespace' => 'Course\Http\Controllers', 'middleware' => ['web', 
     $router->patch('/seasons/{season}/accept', 'SeasonController@accept')->name('seasons.accept');
     $router->patch('/seasons/{season}/reject', 'SeasonController@reject')->name('seasons.reject');
     $router->post('/seasons/{course}', 'SeasonController@store')->name('seasons.store');
+    $router->get('/seasons/{season}/edit', 'SeasonController@edit')->name('seasons.edit');
+    $router->patch('/seasons/{season}/update', 'SeasonController@update')->name('seasons.update');
 });
