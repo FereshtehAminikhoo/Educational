9d3a31b14fce73ecd227fa2c9aaea3fcf664813e
Fereshteh <faminikhoo@gmail.com>
2024-01-27 23:37:32 +0330

    Adding edit and update methods in controller and functions of these in repo

--- modules/Course/Http/Controllers/SeasonController.php
+++ modules/Course/Http/Controllers/SeasonController.php
@@ -22,4 +22,19 @@ class SeasonController extends Controller
         newFeedback();
         return back();
     }
+
+    public function edit($seasonId)
+    {
+        $season = $this->repository->findById($seasonId);
+        return view('Courses::seasons.edit', compact('season'));
+    }
+
+    public function update($seasonId, SeasonRequest $request)
+    {
+        $this->repository->update($seasonId, $request);
+        // Find the course ID associated with the given season ID
+        $courseId = $this->repository->findById($seasonId)->course_id;
+        newFeedback();
+        return redirect(route('courses.details', ['course' => $courseId]));
+    }
 }
--- modules/Course/Repositories/SeasonRepo.php
+++ modules/Course/Repositories/SeasonRepo.php
@@ -10,42 +10,25 @@ class SeasonRepo
 {
     public function store($values, $id)
     {
-        $courseRepo = new CourseRepo();
-        if(is_null($values->number)){
-            $number = $courseRepo->findById($id)->seasons()->orderBy('number', 'desc')->firstOrNew([])->number ?: 0;
-            $number++;
-        }else{
-            $number = $values->number;
-        }
-
         return Season::create([
             'course_id' => $id,
             'user_id' => auth()->id(),
             'title' => $values->title,
-            'number' => $number,
+            'number' => $this->generateNumber($values->number, $id),
             'confirmation_status' => Season::CONFIRMATION_STATUS_PENDING,
         ]);
     }
 
     public function findById($id)
     {
-        return Course::findOrFail($id);
+        return Season::findOrFail($id);
     }
 
     public function update($id, $values)
     {
-        return Course::where('id', $id)->update([
+        return Season::where('id', $id)->update([
             'title' => $values->title,
-            'slug' => Str::slug($values->slug),
-            'priority' => $values->priority,
-            'price' => $values->price,
-            'percent' => $values->percent,
-            'teacher_id' => $values->teacher_id,
-            'type' => $values->type,
-            'status' => $values->status,
-            'category_id' => $values->category_id,
-            'banner_id' => $values->banner_id,
-            'body' => $values->body,
+            'number' => $this->generateNumber($values->number, $id),
         ]);
     }
 
@@ -57,6 +40,22 @@ class SeasonRepo
     public function updateStatus($id, string $status)
     {
         return Course::where('id', $id)->update(['status' => $status]);
+    }
+
+
+
+
+
+
+    public function generateNumber($number, $courseId)
+    {
+        $courseRepo = new CourseRepo();
+        if (is_null($number)) {
+            $number = $courseRepo->findById($courseId)->seasons()->orderBy('number', 'desc')->firstOrNew([])->number ?: 0;
+            $number++;
+        }
+
+        return $number;
     }
 
 }
