143dc12f1af95ac22ea8eff8049e59c049965cd2
Fereshteh <faminikhoo@gmail.com>
2024-01-27 22:23:58 +0330

    Creating and adding season migration and model

---
+++ modules/Course/Models/Season.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Course\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use User\Models\User;
+
+class Season extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+
+    protected $fillable = [
+        'course_id',
+        'user_id',
+        'title',
+        'number',
+        'confirmation_status'
+    ];
+
+
+    const CONFIRMATION_STATUS_ACCEPTED = 'accepted';
+    const CONFIRMATION_STATUS_REJECTED = 'rejected';
+    const CONFIRMATION_STATUS_PENDING = 'pending';
+    static $confirmationStatuses = [self::CONFIRMATION_STATUS_ACCEPTED, self::CONFIRMATION_STATUS_REJECTED, self::CONFIRMATION_STATUS_PENDING];
+
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
