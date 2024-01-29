faa649d47f0736c94f0b097bc9497058eefeb7d0
Fereshteh <faminikhoo@gmail.com>
2024-01-29 23:31:33 +0330

    Adding $fillable and adding values for confirmation_status and status fields

--- modules/Course/Models/Lesson.php
+++ modules/Course/Models/Lesson.php
@@ -13,4 +13,31 @@ class Lesson extends Model
     protected $guarded = [];
 
 
+    protected $fillable = [
+        'course_id',
+        'season_id',
+        'user_id',
+        'media_id',
+        'title',
+        'slug',
+        'free',
+        'body',
+        'time',
+        'priority',
+        'confirmation_status',
+        'status',
+    ];
+
+
+    const CONFIRMATION_STATUS_ACCEPTED = 'accepted';
+    const CONFIRMATION_STATUS_REJECTED = 'rejected';
+    const CONFIRMATION_STATUS_PENDING = 'pending';
+    static $confirmationStatuses = [self::CONFIRMATION_STATUS_ACCEPTED, self::CONFIRMATION_STATUS_REJECTED, self::CONFIRMATION_STATUS_PENDING];
+
+
+    const STATUS_OPENED = 'opened';
+    const STATUS_LOCKED = 'locked';
+    static $statuses = [self::STATUS_OPENED, self::STATUS_LOCKED ];
+
+
 }
