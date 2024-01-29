e3ee709fed5e53049941e21e46ba36e6c49b0714
Fereshteh <faminikhoo@gmail.com>
2024-01-29 17:38:41 +0330

    Adding lesson model

---
+++ modules/Course/Models/Lesson.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Course\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    use HasFactory;
+
+
+    protected $guarded = [];
+
+
+}
