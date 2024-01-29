b6b04af9d7721c4000c70eec60317a95489e160e
Fereshteh <faminikhoo@gmail.com>
2024-01-29 17:37:27 +0330

    Editing the url of season store route

--- modules/Course/Routes/seasons_routes.php
+++ modules/Course/Routes/seasons_routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::group(['namespace' => 'Course\Http\Controllers', 'middleware' => ['web', 'auth', 'verified']], function($router){
-    $router->post('/seasons/{course}/store', 'SeasonController@store')->name('seasons.store');
+    $router->post('/courses/{course}/seasons/store', 'SeasonController@store')->name('seasons.store');
     $router->get('/seasons/{season}/edit', 'SeasonController@edit')->name('seasons.edit');
     $router->patch('/seasons/{season}/update', 'SeasonController@update')->name('seasons.update');
     $router->delete('/seasons/{season}/delete', 'SeasonController@destroy')->name('seasons.destroy');
