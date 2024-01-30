0138fbea8c82ec841c4e37f19fb272dee1e89272
Fereshteh <faminikhoo@gmail.com>
2024-01-30 20:42:27 +0330

    Updating upload method and using mediaFile config in that

--- modules/Media/Services/MediaFileService.php
+++ modules/Media/Services/MediaFileService.php
@@ -9,30 +9,16 @@ class MediaFileService
     public static function upload($file)
     {
         $extension = strtolower($file->getClientOriginalExtension());
-        switch($extension){
-            case 'jpg':
-            case 'png':
-            case'jpeg':
+        foreach(config('mediaFile.MediaTypeServices') as $key => $service){
+            if(in_array($extension, $service['extensions'])){
                 $media = new Media();
-                $media->files = ImageFileService::upload($file);
-                $media->type = 'image';
+                $media->files = $service['handler']::upload($file);
+                $media->type = $key;
                 $media->user_id = auth()->id();
                 $media->filename = $file->getClientOriginalName();
                 $media->save();
                 return $media;
-                break;
-
-            case 'mp4':
-            case 'avi':
-                $media = new Media();
-                $media->files = VideoFileService::upload($file);
-                $media->type = 'video';
-                $media->user_id = auth()->id();
-                $media->filename = $file->getClientOriginalName();
-                $media->save();
-                return $media;
-                break;
-
+            }
         }
     }
 
