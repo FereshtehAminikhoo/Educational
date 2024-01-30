ff91ece78040d09036eeed4c474883a29b571208
Fereshteh <faminikhoo@gmail.com>
2024-01-30 23:03:07 +0330

    Implementing FileServiceContract in this class and writing codes for uploading zip in upload method

--- modules/Media/Services/ZipFileService.php
+++ modules/Media/Services/ZipFileService.php
@@ -2,13 +2,23 @@
 
 namespace Media\Services;
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Media\Contracts\FileServiceContract;
 
-class ZipFileService
+class ZipFileService implements FileServiceContract
 {
-    public static function upload($file)
+    public static function upload(UploadedFile $file) :array
     {
-
+        $filename = uniqid();
+        $extension = $file->getClientOriginalExtension();
+        $dir = 'private\\';
+        Storage::putFileAs($dir, $file, $filename . '.' . $extension);
+        return ['zip' => $dir . $filename . '.' . $extension];
     }
 
+    public static function delete()
+    {
+        // TODO: Implement delete() method.
+    }
 }
