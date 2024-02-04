e95695c349e3a114c50d311835094177cae3aea1
Fereshteh <faminikhoo@gmail.com>
2024-02-04 20:10:15 +0330

    Refactoring media file service

--- modules/Media/Services/MediaFileService.php
+++ modules/Media/Services/MediaFileService.php
@@ -2,22 +2,33 @@
 
 namespace Media\Services;
 
+use Media\Contracts\FileServiceContract;
 use Media\Models\Media;
 
 class MediaFileService
 {
-    public static function upload($file)
+    private static $file;
+    private static $dir;
+    public static function privateUpload($file)
     {
-        $extension = strtolower($file->getClientOriginalExtension());
+        self::$file = $file;
+        self::$dir = 'private/';
+        return self::upload();
+    }
+
+    public static function publicUpload($file)
+    {
+        self::$file = $file;
+        self::$dir = 'public/';
+       return self::upload();
+    }
+
+    private static function upload()
+    {
+        $extension = self::normalizeExtension(self::$file);
         foreach(config('mediaFile.MediaTypeServices') as $key => $service){
             if(in_array($extension, $service['extensions'])){
-                $media = new Media();
-                $media->files = $service['handler']::upload($file);
-                $media->type = $key;
-                $media->user_id = auth()->id();
-                $media->filename = $file->getClientOriginalName();
-                $media->save();
-                return $media;
+                return self::uploadByHandler(new $service['handler'], $key);
             }
         }
     }
@@ -29,5 +40,31 @@ class MediaFileService
                 ImageFileService::delete($media);
                 break;
         }
+    }
+
+    /**
+     * @param $file
+     * @return string
+     */
+    private static function normalizeExtension($file): string
+    {
+        return strtolower($file->getClientOriginalExtension());
+    }
+
+    private static function filenameGenerator()
+    {
+        return uniqid();
+    }
+
+
+    private static function uploadByHandler(FileServiceContract $service, $key): Media
+    {
+        $media = new Media();
+        $media->files = $service::upload(self::$file, self::filenameGenerator(), self::$dir);
+        $media->type = $key;
+        $media->user_id = auth()->id();
+        $media->filename = self::$file->getClientOriginalName();
+        $media->save();
+        return $media;
     }
 }
