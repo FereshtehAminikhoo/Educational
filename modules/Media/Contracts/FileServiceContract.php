0adb56d48e4a14fc79521a2c80e829828676f0d6
Fereshteh <faminikhoo@gmail.com>
2024-02-04 20:09:28 +0330

    Creating file service contract

--- modules/Media/Contracts/FileServiceContract.php
+++ modules/Media/Contracts/FileServiceContract.php
@@ -3,10 +3,11 @@
 namespace Media\Contracts;
 
 use Illuminate\Http\UploadedFile;
+use Media\Models\Media;
 
 interface FileServiceContract
 {
-    public static function upload(UploadedFile $file) :array;
+    public static function upload(UploadedFile $file, string $filename, string $dir) :array;
 
-    public static function delete();
+    public static function delete(Media $media);
 }
