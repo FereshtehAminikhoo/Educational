4b350488c8defbe2ded175472ff634c76ef8ade8
Fereshteh <faminikhoo@gmail.com>
2024-02-05 18:40:20 +0330

    Creating default file service and adding delete method in it

---
+++ modules/Media/Services/DefaultFileService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Media\Services;
+
+use Illuminate\Support\Facades\Storage;
+
+class DefaultFileService
+{
+    public static function delete($media)
+    {
+        foreach($media->files as $file){
+            if($media->is_private){
+                Storage::delete('private\\' . $file);
+            }else{
+                Storage::delete('public\\' . $file);
+            }
+        }
+    }
+}
