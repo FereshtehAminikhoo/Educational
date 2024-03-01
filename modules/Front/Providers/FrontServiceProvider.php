22e901f18e82f16a6b5e2a145fbb8df46508d349
Fereshteh <faminikhoo@gmail.com>
2024-03-01 16:48:55 +0330

    Adding tree method in category repo and writing codes for showing categories and sub categories in service provider

--- modules/Front/Providers/FrontServiceProvider.php
+++ modules/Front/Providers/FrontServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Front\Providers;
 
+use Category\Repositories\CategoryRepo;
 use Illuminate\Support\ServiceProvider;
 
 class FrontServiceProvider extends ServiceProvider
@@ -10,6 +11,11 @@ class FrontServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/front_routes.php');
         $this->loadViewsFrom(__DIR__ . '/../Resources/Views', 'Front');
+
+        view()->composer('Front::layout.header', function($view){
+            $categories = (new CategoryRepo())->tree();
+            $view->with(compact('categories'));
+        });
     }
 
 
