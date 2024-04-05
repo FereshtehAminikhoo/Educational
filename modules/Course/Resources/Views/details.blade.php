78ff97a0bcd3847d21dd7392fd7f3577b8294b92
Fereshteh <faminikhoo@gmail.com>
2024-04-05 15:38:49 +0330

    Debugging if lesson has season or not and what should it show

--- modules/Course/Resources/Views/details.blade.php
+++ modules/Course/Resources/Views/details.blade.php
@@ -48,7 +48,7 @@
                         </td>
                         <td><a href="">{{$lesson->number}}</a></td>
                         <td><a href="">{{$lesson->title}}</a></td>
-                        <td>{{$lesson->season->title}}</td>
+                        <td>{{$lesson->season ? $lesson->season->title : '-'}}</td>
                         <td>{{$lesson->time}} دقیقه </td>
                         <td class="confirmation_status">
                                 <span class="{{$lesson->getConfirmationStatusCssClass()}}">
