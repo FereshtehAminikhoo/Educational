000cd449a2c462b18de72d008a84e9bf861d0251
Fereshteh <faminikhoo@gmail.com>
2024-04-06 16:18:56 +0330

    Adding single course route in front routes and method in controller

--- modules/Front/Http/Controllers/FrontController.php
+++ modules/Front/Http/Controllers/FrontController.php
@@ -3,11 +3,27 @@
 namespace Front\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Course\Repositories\CourseRepo;
+use Illuminate\Support\Str;
 
 class FrontController extends Controller
 {
+    private $repository_course;
+    public function __construct(CourseRepo $courseRepo)
+    {
+        $this->repository_course = $courseRepo;
+    }
+
+
     public function index()
     {
         return view('Front::index');
+    }
+
+    public function singleCourse($slug)
+    {
+        $courseId =  Str::before(Str::after($slug, 'c-'), '-');
+        $course = $this->repository_course->findById($courseId);
+        return view('Front::singleCourse', compact('course'));
     }
 }
--- modules/Front/Routes/front_routes.php
+++ modules/Front/Routes/front_routes.php
@@ -2,4 +2,5 @@
 
 Route::group(['namespace' => 'Front\Http\Controllers', 'middleware' => ['web']], function($router){
     $router->get('/', 'FrontController@index');
+    $router->get('/c-{slug}', 'FrontController@singleCourse')->name('singleCourse');
 });
